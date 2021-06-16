38d986e2b4f015dce0533fcae9341cabfb37bea7
Zehanazani <85845399+Zehanazani@users.noreply.github.com>
2021-06-16 14:35:45 +0700

    update

---
+++ Surat_Keterangan.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Static Navigation - SB Admin</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <!-- Navbar Brand-->
+            <a class="navbar-brand ps-3" href="index.php">GO-RW</a>
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                    <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#!">Settings</a></li>
+                        <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="#!">Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Menu Utama
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Interface</div>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Kategori
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="Surat_Keterangan.php">Surat Keterangan</a>
+                                    <a class="nav-link" href="Keuangan_Warga.php">Keuangan Warga</a>
+                                    <a class="nav-link" href="Pengumuman.php">Pengumuman</a>
+                                    <a class="nav-link" href="Kegiatan.Php">Kegiatan</a>
+                                    <a class="nav-link" href="Pelaporan.Php">Pelaporan</a>
+                                </nav>
+                            </div>
+                            </a>
+                            </a>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+              <main>
+                <div class="card">
+                  <div class="card-header">
+                    <h5>FORM PENGAJUAN SURAT KETERANGAN RW 02
+                  </div>
+                  <div class="card-body">
+                    <table>
+                      <tr>
+                        <td>Nama</td>
+                        <td><input type="text"></td>
+                      </tr>
+                      <tr>
+                        <td>Tempat/Tgl lahir</td>
+                        <td><input type="text"></td>
+                      </tr>
+                      <tr>
+                        <td>No KTP</td>
+                        <td><input type="Number"></td>
+                      </tr>
+                      <tr>
+                        <td>Agama</td>
+                        <td><input type="text"></td>
+                      </tr>
+                      <tr>
+                        <td>Pekerjaan</td>
+                        <td><input type="text"></td>
+                      </tr>
+                      <tr>
+                        <td>Kewarganegaraan</td>
+                        <td><input type="text"></td>
+                      </tr>
+                      <tr>
+                        <td>Jenis kelamin</td>
+                        <td><input type="radio" name="jk"> Laki-Laki
+                            <input type="radio" name="jk"> Perempuan
+                      </tr>
+                      <tr>
+                        <td>Tanggal Pengajuan Surat</td>
+                        <td><input type="date"></td>
+                      </tr>
+                    <table>
+                    <p class="card-text">Catatan</p>
+                    <div class="form-floating">
+                      <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                      <label for="floatingTextarea2">Comments</label>
+                    </div>
+                    <a href="#" class="btn btn-primary">Submit</a>
+                  </div>
+                  <div class="card-footer text-muted">
+                    RW 02 DRAMAGA
+                  </div>
+                </div>
+              </main>
+          </div>
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+      <script src="js/scripts.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+      <script src="assets/demo/chart-area-demo.js"></script>
+      <script src="assets/demo/chart-bar-demo.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+      <script src="js/datatables-simple-demo.js"></script>
+  </body>
+</html>
